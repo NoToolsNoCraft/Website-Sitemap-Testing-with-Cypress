26721f027257d8532217be8fdd0e810898431d1a
NoToolsNoCraft <notoolsnocraft@gmail.com>
2024-12-29 00:00:27 +0100

    Update sitemapMain.cy.js

--- cypress/e2e/No Tools No Craft/sitemapMain.cy.js
+++ cypress/e2e/No Tools No Craft/sitemapMain.cy.js
@@ -47,4 +47,50 @@ describe('Sitemap test for HelloWorld.rs', () => {
           });
       });
 
+    it('All categories sitemap collection', () => {
+        // Request the sitemap file
+        cy.request(`${baseUrl}/category-sitemap.xml`)
+          .its('body')  // Get the response body (XML content)
+          .then((body) => {
+            // Parse the XML response body
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(body, 'text/xml');
+            
+            // Extract all URLs from the <loc> tags
+            const urls = Array.from(xmlDoc.getElementsByTagName('loc')).map((loc) => loc.textContent);
+    
+            // Assert that there are more than 1 URL
+            expect(urls).to.have.length.greaterThan(1);
+    
+            // Visit each URL in the extracted URLs list
+            urls.forEach((url) => {
+              cy.log(url);  // Log each URL for inspection
+              cy.request(url).wait(1000, { log: false });  // Visit each URL with a wait
+            });
+          });
+      });
+
+    it('All pages sitemap collection', () => {
+        // Request the sitemap file
+        cy.request(`${baseUrl}/page-sitemap.xml`)
+          .its('body')  // Get the response body (XML content)
+          .then((body) => {
+            // Parse the XML response body
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(body, 'text/xml');
+            
+            // Extract all URLs from the <loc> tags
+            const urls = Array.from(xmlDoc.getElementsByTagName('loc')).map((loc) => loc.textContent);
+    
+            // Assert that there are more than 1 URL
+            expect(urls).to.have.length.greaterThan(1);
+    
+            // Visit each URL in the extracted URLs list
+            urls.forEach((url) => {
+              cy.log(url);  // Log each URL for inspection
+              cy.request(url).wait(1000, { log: false });  // Visit each URL with a wait
+            });
+          });
+      });
+
   });
